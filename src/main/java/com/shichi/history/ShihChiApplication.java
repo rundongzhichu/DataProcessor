2a52c3a55db01103b46ced982fb339abb1c8a784
rundongzhichu <2298812879@qq.com>
2023-09-10 22:36:02 +0800

    initial commit for shihchi

---
+++ src/main/java/com/shichi/history/ShihChiApplication.java
@@ -0,0 +1,13 @@
+package com.shichi.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShihChiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShihChiApplication.class, args);
+    }
+
+}
