eb3e7142d52fb76778233dbca2ea8489194fb6de
sunshoucai <17396248231@163.com>
2025-05-02 14:44:26 +0800

    fix datasource auto init issue

--- dp-backend/src/main/java/com/dp/backend/DPBackendApplication.java
+++ dp-backend/src/main/java/com/dp/backend/DPBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShihChiApplication {
+public class DPBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShihChiApplication.class, args);
+        SpringApplication.run(DPBackendApplication.class, args);
     }
 
 }
